10e266fa8ff4c477569f824c86d7ee65772e4272
SunRise <64601954+massyAch06@users.noreply.github.com>
2021-11-10 00:21:53 +0100

    IA-v1

---
+++ IA-BOT/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace IA_BOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.richTextInput = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.richTextInput);
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 248);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(576, 102);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // richTextInput
+            // 
+            this.richTextInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextInput.Location = new System.Drawing.Point(3, 3);
+            this.richTextInput.Name = "richTextInput";
+            this.richTextInput.Size = new System.Drawing.Size(445, 48);
+            this.richTextInput.TabIndex = 0;
+            this.richTextInput.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Envoyer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 350);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.RichTextBox richTextInput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IA-BOT/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace IA_BOT
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Choices list = new Choices();
+        RichTextBox rbox = new RichTextBox();
+        
+
+        string c1 = "salut";
+        string c2 = "comment va tu";
+        string c3 = "il est quel heure";
+        string c4 = "on est quelle date";
+        public Form1()
+        {
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+            list.Add(c1);
+            list.Add(c2);
+            list.Add(c3);
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += Rec_SpeechRecognized; ;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { }
+            s.SelectVoiceByHints(VoiceGender.Female);
+            InitializeComponent();
+        }
+
+        public void say(string h)
+        {
+            s.Speak(h);
+        }
+        private void Rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string r = e.Result.Text;
+
+            if (r == "Salut")
+            {
+                say("salut");
+                
+            }
+            if (r == "comment va tu")
+            {
+                say("bien, et vous");
+                
+            }
+            if (r == c3)
+            {
+                say("il est " + DateTime.Now.ToString("H:m:tt"));
+            }
+            if (r == c4)
+            {
+                say("on est le " + DateTime.Now.ToString("dd/MM/yyyy"));
+            }
+            if (r == "google")
+            {
+                Process.Start("www.google.com");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            rbox.Location = new Point(236, 97);
+            rbox.ForeColor = Color.Black;
+
+            //rbox.Text = "!..Welcome to GeeksforGeeks..!";
+            this.Controls.Add(rbox);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tr = richTextInput.Text;
+
+            if (tr == c1) { 
+                say("salut");
+                rbox.Text = "Salut"; 
+            }
+            
+            if (tr == c2) { 
+                say("bien, et vous");
+                rbox.Text = "Bien et vous?"; 
+            }
+            if (tr == c3)
+            {
+                
+                say("il est "+ DateTime.Now.ToString("H:m:tt"));
+                rbox.Text = "il est " + DateTime.Now.ToString("H:m:tt");
+            }
+            if (tr == c4)
+            {
+                say("on est le " + DateTime.Now.ToString("dd/MM/yyyy"));
+                rbox.Text = "on est le " + DateTime.Now.ToString("dd/MM/yy");
+            }
+            if (tr == "google")
+            {
+                Process.Start("www.google.com");
+                //Process.Start("chrome.exe");
+            }
+            richTextInput.Clear();
+        }
+    }
+}
