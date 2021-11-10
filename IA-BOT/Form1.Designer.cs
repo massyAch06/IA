c0070aa1c6b86d4f846a232940cdd4078ced0f89
SunRise <64601954+massyAch06@users.noreply.github.com>
2021-11-10 01:09:44 +0100

    IA BOT v1.1

--- IA-BOT/Form1.Designer.cs
+++ IA-BOT/Form1.Designer.cs
@@ -29,59 +29,75 @@ namespace IA_BOT
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.richTextInput = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.flowLayoutPanel2.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.Controls.Add(this.richTextInput);
-            this.flowLayoutPanel2.Controls.Add(this.button1);
-            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 248);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(576, 102);
-            this.flowLayoutPanel2.TabIndex = 1;
             // 
             // richTextInput
             // 
             this.richTextInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextInput.Location = new System.Drawing.Point(3, 3);
+            this.richTextInput.Location = new System.Drawing.Point(12, 265);
             this.richTextInput.Name = "richTextInput";
-            this.richTextInput.Size = new System.Drawing.Size(445, 48);
+            this.richTextInput.Size = new System.Drawing.Size(473, 48);
             this.richTextInput.TabIndex = 0;
             this.richTextInput.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(454, 3);
+            this.button1.Location = new System.Drawing.Point(491, 265);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 48);
+            this.button1.Size = new System.Drawing.Size(73, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "Envoyer";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IA-BOT v1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(389, 325);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(175, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/massyAch06/IA";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 350);
-            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.RichTextBox richTextInput;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- IA-BOT/Form1.cs
+++ IA-BOT/Form1.cs
@@ -20,16 +20,35 @@ namespace IA_BOT
         RichTextBox rbox = new RichTextBox();
         
 
-        string c1 = "salut";
-        string c2 = "comment va tu";
+        string c1 = "salut"; 
+        string c8 = "coucou"; 
+        string c9 = "Bonjour"; 
+        string c10 = "Bonsoir"; 
+        string c11 = "cc";
+        string c2 = "comment va tu"; 
+        string c12 = "bien"; 
+        string c13 = "merci";
         string c3 = "il est quel heure";
         string c4 = "on est quelle date";
+        string c5 = "recherche google";
+        string c6 = "ouvre google chrome"; 
+        string c7 = "chrome";
         public Form1()
         {
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
             list.Add(c1);
             list.Add(c2);
             list.Add(c3);
+            list.Add(c4);
+            list.Add(c5);
+            list.Add(c6); 
+            list.Add(c7);
+            list.Add(c8);
+            list.Add(c9);
+            list.Add(c10);
+            list.Add(c11);
+            list.Add(c12);
+            list.Add(c13);
             Grammar gr = new Grammar(new GrammarBuilder(list));
             try
             {
@@ -52,16 +71,25 @@ namespace IA_BOT
         {
             string r = e.Result.Text;
 
-            if (r == "Salut")
+            if (r == c1 || r == c8 || r == c9 || r == c10 || r == c11)
             {
                 say("salut");
                 
             }
             if (r == "comment va tu")
             {
-                say("bien, et vous");
+                say("bien, et vous?");
                 
             }
+            if (r == c12)
+            {
+                say("content de l'entendre");
+            }
+            if (r == c13)
+            {
+                say("de rien");
+            }
+
             if (r == c3)
             {
                 say("il est " + DateTime.Now.ToString("H:m:tt"));
@@ -70,19 +98,26 @@ namespace IA_BOT
             {
                 say("on est le " + DateTime.Now.ToString("dd/MM/yyyy"));
             }
-            if (r == "google")
+            if (r == c5)
             {
                 Process.Start("www.google.com");
             }
+            if (r == c6 || r == c7)
+            {
+                Process.Start("chrome.exe");
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
            
 
-            rbox.Location = new Point(236, 97);
+            rbox.Location = new Point(13, 13);
             rbox.ForeColor = Color.Black;
-
+            rbox.ReadOnly = true;
+            rbox.BorderStyle = BorderStyle.None;
+            rbox.Size = new Size(550, 230);
             //rbox.Text = "!..Welcome to GeeksforGeeks..!";
             this.Controls.Add(rbox);
         }
@@ -91,14 +126,22 @@ namespace IA_BOT
         {
             string tr = richTextInput.Text;
 
-            if (tr == c1) { 
-                say("salut");
-                rbox.Text = "Salut"; 
+            if (tr == c1 || tr == c8 || tr == c9 || tr == c10 || tr == c11) { 
+                say(c1);
+                rbox.Text = c1; 
             }
             
             if (tr == c2) { 
                 say("bien, et vous");
                 rbox.Text = "Bien et vous?"; 
+            }
+            if (tr == c12)
+            {
+                say("content de l'entendre");
+            }
+            if (tr == c13)
+            {
+                say("de rien");
             }
             if (tr == c3)
             {
@@ -111,12 +154,21 @@ namespace IA_BOT
                 say("on est le " + DateTime.Now.ToString("dd/MM/yyyy"));
                 rbox.Text = "on est le " + DateTime.Now.ToString("dd/MM/yy");
             }
-            if (tr == "google")
+            if (tr == c5)
             {
                 Process.Start("www.google.com");
-                //Process.Start("chrome.exe");
+                
+            }
+            if (tr == c6 || tr == c7)
+            {
+                Process.Start("chrome.exe");
             }
             richTextInput.Clear();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/massyAch06/IA");
         }
     }
 }
